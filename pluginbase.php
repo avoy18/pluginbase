cdea382406f1eabbcfd66f7b98a36e98aeb7a594
anton152 <anton@ictin.us>
2021-04-24 21:55:37 -0400

    Remove WP Update Server Files

--- pluginbase.php
+++ pluginbase.php
@@ -6,8 +6,8 @@
 /**
  * Plugin Name: Base Plugin
  * Plugin URI: https://base.plugin
- * Description: Plugin starter files
- * Version: 1.0.0
+ * Description: Plugin starter files and the version is 1.2.0
+ * Version: 1.9.0
  * Author: Anton Voytenko
  * Author URI: https://avoy.me
  * License: MTI
@@ -16,36 +16,6 @@
 if (!defined('ABSPATH') && !function_exists('add_action')) {
     die;
 }
-
-/**
- * Selectively uncomment the sections below to enable updates with WP Plugin Update Server.
- *
- * WARNING - READ FIRST:
- * Before deploying the plugin or theme, make sure to change the following value
- * - https://your-update-server.com  => The URL of the server where WP Plugin Update Server is installed
- * - $prefix_updater                 => Replace "prefix" in this variable's name with a unique plugin prefix
- *
- * @see https://github.com/froger-me/wp-package-updater
- **/
-
-require_once plugin_dir_path(__FILE__) . 'lib/wp-package-updater/class-wp-package-updater.php';
-
-/** Enable plugin updates with license check **/
-// $prefix_updater = new WP_Package_Updater(
-// 	'https://your-update-server.com',
-// 	wp_normalize_path( __FILE__ ),
-// 	wp_normalize_path( plugin_dir_path( __FILE__ ) ),
-// 	true
-// );
-
-/** Enable plugin updates without license check **/
-$prefix_updater = new WP_Package_Updater(
-	'https://your-update-server.com',
-	wp_normalize_path( __FILE__ ),
-	wp_normalize_path( plugin_dir_path( __FILE__ ) )
-);
-
-/* ================================================================================================ */
 
 function dummy_plugin_run()
 {
